9f551ff1af3d2165172cccf089f6993c0718b1c8
racoltdev <racoltdev@gmail.com>
2025-12-22 17:39:34 -0500

    cleanup

--- dummmy_malloc/dummy_malloc.c
+++ dummmy_malloc/dummy_malloc.c
@@ -6,10 +6,15 @@
 #include <stdio.h>  // printf
 
 void* malloc(size_t size) {
-	void* (*libc_malloc)(size_t) = (void* (*)(size_t))dlsym(RTLD_NEXT, "malloc");
+	void* (*libc_malloc)(size_t);
+	void* (*libc_exit)(int);
+
+	*(void **) (&libc_malloc) = dlsym(RTLD_NEXT, "malloc");
+	*(void **) (&libc_exit) = dlsym(RTLD_NEXT, "exit");
+
 	fprintf(stderr, "malloc(%zu)\n", size);
 	void* address = libc_malloc(size);
 	fprintf(stderr, "Address:%p\n", address);
-	void (*libc_exit)(int) = (void (*)(int))dlsym(RTLD_NEXT, "exit");
 	libc_exit(0);
+	return address;
 }
